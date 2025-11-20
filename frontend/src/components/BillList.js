d94c94000b2cdb0d086b05bc127b3b7bb57283f1
Yenuli <yenuli.sirisena@yahoo.com>
2025-11-20 19:21:08 +0530

    Update

--- frontend/src/components/BillList.js
+++ frontend/src/components/BillList.js
@@ -75,7 +75,7 @@ const BillList = () => {
 
   const getMonthName = (monthNumber) => {
     const months = [
-      'January1', 'February', 'March', 'April', 'May', 'June',
+      'January', 'February', 'March', 'April', 'May', 'June',
       'July', 'August', 'September', 'October', 'November', 'December'
     ];
     return months[monthNumber - 1];
