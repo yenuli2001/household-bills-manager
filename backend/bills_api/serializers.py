1a565d2d525896655471e4e419d40289f4b7314d
Yenuli <yenuli.sirisena@yahoo.com>
2025-11-22 09:01:15 +0530

    commit

--- backend/bills_api/serializers.py
+++ backend/bills_api/serializers.py
@@ -6,7 +6,7 @@ class BillSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Bill
-        fields = ['id', 'bill_type', 'amount', 'discount', 'final_amount', 'date', 'description', 'created_at', 'updated_at', 'user_id']
+        fields = ['id', 'bill_type', 'amount', 'discount', 'final_amount', 'date', 'description', 'created_at', 'updated_at']
     
     def validate_discount(self, value):
         if value < 0 or value > 100:
